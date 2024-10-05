842dd522306edbd11ee4f6d8e907a607dadbefb2
JuanDT24 <tiradodj@uninorte.edu.co>
2024-10-05 00:57:53 -0500

    Merged both endpoints, the default one and the hamburger one

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HamburgerController } from './Hamburger/hamburger.controller';
 import { HamburgerModule } from './Hamburger/hamburger.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
-  imports: [HamburgerModule],
-  controllers: [],
-  providers: [],
+  imports: [HamburgerModule, AppModule],
+  controllers: [AppController, HamburgerController],
+  providers: [AppService],
 })
 export class AppModule {}
